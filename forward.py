6faa681cc0767864558b9944562bd7bb152b1e6d
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-04-10 11:44:05 -0700

    commit

--- forward.py
+++ forward.py
@@ -13,12 +13,12 @@ debug_group = bot.get_chat(-1001198682178)
 channel = bot.get_chat('@web_record')
 
 def onMessage(message):
-	print(message)
+    print(message)
 
 Wechaty.instance() // Global Instance
-  .on('scan', lambda qrcode, status : print('Scan QR Code to login: {}\nhttps://api.qrserver.com/v1/create-qr-code/?data={}'.format(status, encodeURIComponent(qrcode))))
-  .on('login', lambda user: print('User {} logined'.format(user)))
-  .on('message', lambda message: onMessage(message))
-  .start()
+    .on('scan', lambda qrcode, status : print('Scan QR Code to login: {}\nhttps://api.qrserver.com/v1/create-qr-code/?data={}'.format(status, encodeURIComponent(qrcode))))
+    .on('login', lambda user: print('User {} logined'.format(user)))
+    .on('message', lambda message: onMessage(message))
+    .start()
 
 
