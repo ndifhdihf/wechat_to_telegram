2e6b620b8e3576708189233a452a544fdad26446
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-04-10 11:42:26 -0700

    commit

--- forward.py
+++ forward.py
@@ -2,20 +2,23 @@
 # -*- coding: utf-8 -*-
 
 from telegram.ext import Updater
-
 from wechaty import Wechaty
-
-Wechaty.instance() // Global Instance
-  .on('scan', lambda qrcode, status : print('Scan QR Code to login: {}\nhttps://api.qrserver.com/v1/create-qr-code/?data={}'.format(status, encodeURIComponent(qrcode))))
-  .on('login', lambda user: print('User {} logined'.format(user)))
-  .on('message', lambda message: print('Message: {}'.format(message)))
-  .start()
 
 def getFile(name):
     with open(name) as f:
         return yaml.load(f, Loader=yaml.FullLoader)
 
-credential = getFile('credential')
-
-bot = Updater(credential['bot_token'], use_context=True).bot
+bot = Updater(getFile('credential')['bot_token'], use_context=True).bot
 debug_group = bot.get_chat(-1001198682178)
+channel = bot.get_chat('@web_record')
+
+def onMessage(message):
+	print(message)
+
+Wechaty.instance() // Global Instance
+  .on('scan', lambda qrcode, status : print('Scan QR Code to login: {}\nhttps://api.qrserver.com/v1/create-qr-code/?data={}'.format(status, encodeURIComponent(qrcode))))
+  .on('login', lambda user: print('User {} logined'.format(user)))
+  .on('message', lambda message: onMessage(message))
+  .start()
+
+
--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import os
 import sys
 
 def kill():
-	os.system("ps aux | grep ython | grep daily_summary | awk '{print $2}' | xargs kill -9")
+	os.system("ps aux | grep ython | grep forward | awk '{print $2}' | xargs kill -9")
 
 def setup():
 	kill()
 	if 'kill' in str(sys.argv):
 		return 
 	addtional_arg = ' '.join(sys.argv[1:])
-	command = 'python3 -u daily_summary.py %s' % addtional_arg
+	command = 'python3 -u forward.py %s' % addtional_arg
 	os.system('nohup %s & && tail -F nohup.out' % command)
 
 if __name__ == '__main__':
