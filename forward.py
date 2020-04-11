20ef3349bcbe5cd6e6d1db36bb8980da520b80ff
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-04-11 07:05:14 -0700

    commit

--- forward.py
+++ forward.py
@@ -21,7 +21,8 @@ link_status = {}
 def text_reply_imp(msg):
 	if not msg.Url:
 		return
-	if link_status.get(msg.FileName, 0) >= 1:
+	link_status[msg.FileName] = link_status.get(msg.FileName, 0)
+	if link_status.get[msg.FileName] >= 1:
 		return # sent before
 	if matchKey(msg.User.NickName, ['女权', '平权', 'hardcore', 'dykes']):
 		link_status[msg.FileName] += 1
