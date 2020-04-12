c499b37f5d791bcc31ef20ba1d76d96ac59ece15
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-04-12 10:37:33 -0700

    commit

--- reply.py
+++ reply.py
@@ -13,29 +13,26 @@ debug_group = bot.get_chat(-1001198682178)
 
 @log_on_fail(debug_group)
 def bot_group(update, context):
-	print(1)
 	msg = update.message
 	if not msg:
 		return
-	print(2)
 	if msg.chat_id != debug_group.id or not msg.text:
 		return
-	print(3)
 	r_msg = msg.reply_to_message
 	if not r_msg:
 		return
-	print(4)	
 	cap = r_msg.text or r_msg.caption
-	print(cap)
 	if not cap:
 		return
 	name = cap.split(':')[0].split(' ')[-1]
-	print(name)
 	contact = Contact()
 	if name not in contact.contact:
 		return
-	print(msg.text)
-	itchat.send(msg.text, toUserName=contact.contact[name])
+	try:
+		itchat.send(msg.text, toUserName=contact.contact[name])
+	except:
+		itchat.auto_login(enableCmdQR=2, hotReload=True)
+		itchat.send(msg.text, toUserName=contact.contact[name])
 
 tele.dispatcher.add_handler(MessageHandler(Filters.group, bot_group), group = 3)
 tele.start_polling()
