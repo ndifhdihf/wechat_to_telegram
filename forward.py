71ec8dbaa09110006c9258ef9a0526988fde2a7c
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-06-03 18:07:48 -0700

    commit

--- forward.py
+++ forward.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 BLACKLIST = ['waerrpage', 'MzIwOTkzNzQ0MQ', 'Panziye4869', 'Artemis', 
-	'support.weixin.qq.com', 'volition']
+	'support.weixin.qq.com', 'volition', 'MzI5NzUxMDU4OQ']
 
 from telegram.ext import Updater
 import itchat
