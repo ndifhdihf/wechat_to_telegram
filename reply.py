6430a182f580c23242fe9407a83a60db0cef9b6b
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-07-09 10:06:59 -0700

    commit

--- reply.py
+++ reply.py
@@ -34,8 +34,8 @@ def login():
 def sendToFeminismPrivateGroup(msg):
 	if msg.from_user.first_name != 'Yunz':
 		prefix = 'From ' + msg.from_user.first_name + ': '
-	print(itchat.search_chatrooms(name = wechat_feminism_group_name))
-	chatroom_id = itchat.search_chatrooms(name = wechat_feminism_group_name)[0].id
+	chatroom_id = itchat.search_chatrooms(
+		name = wechat_feminism_group_name)[0]['UserName']
 	if msg.text:
 		itchat.send(prefix + msg.text, chatroom_id)
 		return
