b5e46cb29d3b9e803a4607b2d6ef2741602b6c80
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-07-21 16:16:31 -0700

    commit

--- forward.py
+++ forward.py
@@ -5,7 +5,7 @@ BLACKLIST = ['waerrpage', 'MzIwOTkzNzQ0MQ', 'Panziye4869', 'Artemis',
 	'support.weixin.qq.com', 'volition', 'MzI5NzUxMDU4OQ', '/promo/', 
 	'MzI0ODg4NDM5Mw', 'MzAwNjgzMTQ5NQ', 'MzIyOTQyNDY2OQ', 'MzU2ODAyMTc3MQ',
 	'MzU4NTc3NzA4Mg', 'MzU1MzgyMzg4Mg', 
-	'MzU0NTI2OTk5MA', 'MzI5OTIzNjE3OA', # 讲座信息
+	# 'MzU0NTI2OTk5MA', 'MzI5OTIzNjE3OA', # 讲座信息
 	'MzIxNzA3NDQ5NQ',
 ]
 
