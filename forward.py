70388429160bad56439cd805ddb0cf5483ea26e9
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-08-04 06:30:35 -0700

    commit

--- forward.py
+++ forward.py
@@ -11,6 +11,7 @@ BLACKLIST = ['waerrpage', 'MzIwOTkzNzQ0MQ', 'Panziye4869', 'Artemis',
 	'MzUyMTMzODAzMQ', 'MzI3ODE0Nzk4Mw'
 ]
 
+
 LECTURE_KEYS = ['MzU0NTI2OTk5MA', 'MzI5OTIzNjE3OA', '讲座']
 
 from telegram.ext import Updater
